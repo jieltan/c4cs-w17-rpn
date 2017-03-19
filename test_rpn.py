4a8ef9d4a1ec81c70e4df447d1909793807f0061
jieltan <jieltan@umich.edu>
2017-03-18 21:07:12 -0400

    wth is going on

--- test_rpn.py
+++ test_rpn.py
@@ -7,3 +7,7 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+	def test_exponent(self):
+		result = rpn.calculate('2 6 ^')
+		self.assertEqual(64, result)
+	
