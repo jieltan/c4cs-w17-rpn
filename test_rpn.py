c04b319e57be6c25f91160b1489afc68b6d7f48a
jieltan <jieltan@umich.edu>
2017-03-24 13:52:45 -0400

    w.e.:

--- test_rpn.py
+++ test_rpn.py
@@ -1,5 +1,6 @@
 import unittest
 import rpn
+import colored
 class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
@@ -10,8 +11,8 @@ class TestBasics(unittest.TestCase):
 	def test_exponent(self):
 		result = rpn.calculate('2 6 ^')
 		self.assertEqual(64, result)
-		print("%s Result: %s", result, (fg('red'),attr('reset')))
+		print("%s Result: %s", result, (colored.fg('red'),colored.attr('reset')))
 	def test_exponent(self):
 		result = rpn.calculate('2 2 ^')
 		self.assertEqual(4, result)
-		print("%s Result: %s", result, (fg('red'),attr('reset')))
+		print("%s Result: %s", result, (colored.fg('red'),colored.attr('reset')))
